3815d29de1177f698d49296517c0ae4f5a15960d
Percyana552 <fernadaguilhermekulyumba@gmail.com>
2023-12-21 01:11:12 +0400

    Vending machine.py

--- Vending machine.py
+++ Vending machine.py
@@ -1,4 +1,4 @@
-# make a list of items that  vending machine should display
+# make a list of vending machine to display on the screen
 items = {
     "drinks": [
         {'code': 1, 'name': 'pepsi', 'price': 8, 'stock': 10},
@@ -28,17 +28,16 @@ items = {
         {'code': 20, 'name': 'cappuccino', 'price': 5, 'stock': 13},
         {'code': 21, 'name': 'Americano', 'price': 14, 'stock': 2}
     ] 
-    }
+}
+
 print("Welcome to Petra's vending machine")
 for category, category_items in items.items():
     print(category)
     for item in category_items:
         print(f"Code: {item['code']}, Name: {item['name']}, Price: {item['price']}, Stock: {item['stock']}")
 
-user_item_code =int(input("Enter code (Enter 0 to Exit or 00 to add more items:)")) 
-
-# here we are going to write vending machine code.
-num_vending_machine=2
+user_item_code = int(input("Enter the code of the item you want to buy (Enter 0 to exit or 00 to  add more items):"))
+# vending machine-code
 while user_item_code != 0:
     item = None
     for category in items.values():
@@ -52,33 +51,34 @@ while user_item_code != 0:
     if item:
         if item['stock'] > 0:
             print(f"Wonderful, {item['name']} will cost you {item['price']} AED, stock left: {item['stock']}")
-            payment_method = input("Enter 2  if you would love to pay by cash or 3 if would like to pay by credit card: ")
-            if payment_method == 3:
-                print(" executing  credit  trasction ...")
-                # here you would add credit card password
-                print(" your payment is sucessful!")
-            else:
-                user_money = int(input(f"Enter {item['price']} AED to buy : "))
+            payment_method = input("Enter '2' if you would love to pay by cash or '3' if would like to pay by credit card: ")
+            if payment_method == '3':
+                user_money = int(input(f"Enter the amount you would like to pay with your credit card: "))
                 while user_money < item['price']:
                     print("You don't have enough money for purchase this item.")
                     add_money = int(input(f"Please enter to add more money {item['price']-user_money} AED that you would like to Purchase: "))
                     user_money += add_money
                 change = user_money - item['price']
                 item['stock'] -= 1
-                print(f"Thank you for purchase here is your {item['name']}. Stock left:{item['stock']},your change is {change}")
+                print(f"Executing credit transaction...")
+                print(f"Your payment is successful! Here is your {item['name']}. Stock left:{item['stock']}, your change is {change} AED")
+            elif payment_method == '2':
+                user_money = int(input(f"Enter the amount you would like to pay with cash: "))
+                while user_money < item['price']:
+                    print("You don't have enough money to purchase this item.")
+                    add_money = int(input(f"Please enter to add more money {item['price']-user_money} AED that you would like to Purchase: "))
+                    user_money += add_money
+                change = user_money - item['price']
+                item['stock'] -= 1
+                print(f"Executing cash transaction...")
+                print(f"Your payment is successful! Here is your {item['name']}. Stock left:{item['stock']}, your change is {change} AED")
         else:
-            print("Sorry, this item is out of stock.")
+            print(f"Sorry, {item['name']} is out of stock.")
     else:
-        print("The item code you entered doesn't exist.")
-    print("\n another item you would like to buy:")
-    for category, category_items in items.items():
-        for item in category_items:
-            print(f"Code: {item['code']}, Name: {item['name']}, Price: {item['price']}, Stock: {item['stock']}")
-            print(f"\n There are (num_vending_machine) vending machine available.")
-user_item_code = int(input("Enter code (Enter 0 to Exit or 00 to add more items:)"))
-    
+        print("Invalid item code.")
+    user_item_code = int(input("Enter the code of the item you want to buy (Enter 0 to exit or 00 to  add more items):"))
+print("Thank you for using Petras' vending machine!")
 #  first you should make list in a way to give output the suggestios to the user.
-
 suggestions = {
     1: "cookies",
     2: "Donuts",
